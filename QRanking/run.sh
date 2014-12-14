28fd43ea4281e27259d2fcdf641bd966f3abb552
wangliang <wangliangpeking@gmail.com>
2014-12-14 19:07:30 +0800

    Update script

--- QRanking/run.sh
+++ QRanking/run.sh
@@ -4,9 +4,9 @@ rm edu/pku/QRanking/*/*.class
 javac -encoding UTF-8 -classpath "jsoup-1.8.1.jar:seg.jar:stanford-postagger-3.2.0.jar:." edu/pku/QRanking/AnswerExtraction.java
 
 echo 'run ranking algorithm on wikipedia data...'
-java -classpath "jsoup-1.8.1.jar:seg.jar:stanford-postagger-3.2.0.jar:." edu.pku.QRanking.AnswerExtraction stage3.xml stage4_wiki.txt > out.txt 2>&1 &
+java -classpath "jsoup-1.8.1.jar:seg.jar:stanford-postagger-3.2.0.jar:." -jar QRanking stage3.xml stage4_wiki.txt > out.txt 2>&1 &
 
 
 echo 'run ranking algorithm on crawler data...'
-java -classpath "jsoup-1.8.1.jar:seg.jar:stanford-postagger-3.2.0.jar:." edu.pku.QRanking.AnswerExtraction baidu_crawler_data_v5.xml stage4_open.txt > out.txt 2>&1 &
+java -classpath "jsoup-1.8.1.jar:seg.jar:stanford-postagger-3.2.0.jar:." -jar QRanking baidu_crawler_data_v5.xml stage4_open.txt > out.txt 2>&1 &
 
